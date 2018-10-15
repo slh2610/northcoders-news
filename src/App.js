025c506dfb9010a6ee2bd2e0f356b43819142e24
slh1984 <sallyharrison1984@hotmail.com>
2018-10-15 12:34:18 +0100

    React app set-up and grid template created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <p>Northcoder's News</p>
         </header>
+        <nav>
+          <p>Nav Bar</p>
+        </nav>
+        <button>
+          login
+        </button>
+        <sidebar className="sidebar">
+          <p>Popular articles</p>
+        </sidebar>
+        <main>
+          <p>Body</p>
+        </main>
+        <footer>
+          <p>extra info</p>
+        </footer>
       </div>
     );
   }
